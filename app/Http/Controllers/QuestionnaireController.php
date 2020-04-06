76d59939ac30142f501c84faeaee0f38e3308e87
Yuriy Piddubnyak <banzaj@gmail.com>
2020-04-06 18:15:07 +0300

    Complete project

--- app/Http/Controllers/QuestionnaireController.php
+++ app/Http/Controllers/QuestionnaireController.php
@@ -34,7 +34,7 @@ class QuestionnaireController extends Controller
 
     public function show(\App\Questionnaire $questionnaire) {
 
-        $questionnaire->load('questions.answers');
+        $questionnaire->load('questions.answers.responses');
 
         return view('questionnaire.show', compact('questionnaire'));
     }
